b4f2fa86c00cf529cdb9e11edfa1c4ea784edc68
Joy Hauser <joy.hauser@hotmail.com>
2018-02-01 12:29:57 -0600

    Update web server to use a function to serve files asynchronously

--- server.js
+++ server.js
@@ -9,17 +9,30 @@ const http = require('http');
 // Import the fs library (to get the file system)
 const fs = require('fs');
 
+function serveFile(path, res) {
+    fs.readFile(path, function(err, data) {
+        if(err) {
+            console.log(err);
+            res.statusCode = 500;
+            res.end("Server Error: Could not read file");
+            return;
+        }
+        
+        res.end(data);
+    })
+}
+
 function handleRequest(req, res) {
     switch(req.url) {
         case '/':
         case '/openhouse.html':
-            res.end(fs.readFileSync('public/openhouse.html'));
+            serveFile('public/openhouse.html', res);
             break;
         case '/openhouse.css':
-            res.end(fs.readFileSync('public/openhouse.css'));
+            serveFile('public/openhouse.css', res);
             break;
         case '/openhouse.js':
-            res.end(fs.readFileSync('public/openhouse.js'));
+            serveFile('public/openhouse.js', res);
             break;
         default:
             res.statusCode = 404;
